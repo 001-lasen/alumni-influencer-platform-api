d8fc16f1bbb62b7a26e6480c11d66ee31406a744
Lasen Punyawardana <lpunyawardana@gmail.com>
2026-02-21 11:09:48 +0530

    feat: creating JWT tokens

--- src/utils/environmentConstants.js
+++ src/utils/environmentConstants.js
@@ -1,3 +1,5 @@
 module.exports = {
-    BYCRYPT_SALT_ROUNDS: process.env.BCRYPT_ROUNDS
+    BYCRYPT_SALT_ROUNDS: process.env.BCRYPT_ROUNDS,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION,
 };
