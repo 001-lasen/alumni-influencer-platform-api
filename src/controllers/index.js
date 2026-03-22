fe07507b1df584a21eea9b173ea307244e1c39b1
Lasen Punyawardana <lpunyawardana@gmail.com>
2026-03-22 10:24:31 +0530

    feat: creating verify user email feature

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,11 +4,13 @@ const buildCreateUserController = require('./users/createUser.controller');
 const buildHashDataController = require('./utilities/hashData.controller');
 const buildUserLoginController = require('./users/userLogin.controller');
 const buildResendOTPController = require('./users/resendOTP.controller');
+const buildVerifyUserEmailController = require('./users/verifyUserEmail.controller');
 
 //user controllers
 module.exports.createUser = buildCreateUserController(services.createUser);
 module.exports.userLogin = buildUserLoginController(services.userLogin);
 module.exports.resendOTP = buildResendOTPController(services.verifyEmail);
+module.exports.verifyUserEmail = buildVerifyUserEmailController(services.verifyEmail);
 
 //utilities controllers
 module.exports.hashData = buildHashDataController(services.hashData);
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const callback = require('../utils/callback');
-const { createUser, userLogin, resendOTP} = require('../controllers');
+const { createUser, userLogin, resendOTP, verifyUserEmail} = require('../controllers');
 
 router.post('/create-user', callback(createUser));
 router.post('/login', callback(userLogin));
 router.get('/resend-otp', callback(resendOTP));
+router.post('/verify-email', callback(verifyUserEmail));
 
 module.exports = router;
